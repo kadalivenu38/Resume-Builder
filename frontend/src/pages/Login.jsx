81580ac2fa5be7abba89accf2c10f873aaa0820c
Venu <kadalivenu38@gmail.com>
2026-04-25 21:37:03 +0530

    Login and Navbar Added.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,10 @@
+import { Mail, User2Icon, Lock } from 'lucide-react'
 import React from 'react'
 
 const Login = () => {
-  const [state, setState] = React.useState("login")
+  const query = new URLSearchParams(window.location.search)
+  const urlstate = query.get('state')
+  const [state, setState] = React.useState(urlstate || "login")
 
   const [formData, setFormData] = React.useState({
     name: '',
@@ -28,38 +31,38 @@ const Login = () => {
           {state === "login" ? "Login" : "Sign up"}
         </h1>
 
-        <p className="text-gray-400 text-sm mt-2">Please sign in to continue</p>
+        <p className="text-gray-400 text-sm mt-2">Please {state} to continue</p>
 
         {state !== "login" && (
           <div className="flex items-center mt-6 w-full bg-gray-800 border border-gray-700 h-12 rounded-full overflow-hidden pl-6 gap-2 ">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="text-gray-400" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"> <circle cx="12" cy="8" r="5" /> <path d="M20 21a8 8 0 0 0-16 0" /> </svg>
+            <User2Icon size={16} color='#6B7280' />
             <input type="text" name="name" placeholder="Name" className="w-full bg-transparent text-white placeholder-gray-400 border-none outline-none " value={formData.name} onChange={handleChange} required />
           </div>
         )}
 
         <div className="flex items-center w-full mt-4 bg-gray-800 border border-gray-700 h-12 rounded-full overflow-hidden pl-6 gap-2 ">
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="text-gray-400" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"> <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" /> <rect x="2" y="4" width="20" height="16" rx="2" /> </svg>
+          <Mail size={16} color='#6B7280' />
           <input type="email" name="email" placeholder="Email id" className="w-full bg-transparent text-white placeholder-gray-400 border-none outline-none " value={formData.email} onChange={handleChange} required />
         </div>
 
         <div className=" flex items-center mt-4 w-full bg-gray-800 border border-gray-700 h-12 rounded-full overflow-hidden pl-6 gap-2 ">
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="text-gray-400" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"> <rect width="18" height="11" x="3" y="11" rx="2" ry="2" /> <path d="M7 11V7a5 5 0 0 1 10 0v4" /> </svg>
+          <Lock size={16} color='#6B7280' />
           <input type="password" name="password" placeholder="Password" className="w-full bg-transparent text-white placeholder-gray-400 border-none outline-none" value={formData.password} onChange={handleChange} required />
         </div>
 
         <div className="mt-4 text-left">
-          <button className="text-sm text-indigo-400 hover:underline">
+          <button className="text-sm text-green-400 hover:underline">
             Forget password?
           </button>
         </div>
 
-        <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-indigo-600 hover:bg-indigo-500 transition " >
+        <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-green-600 hover:bg-green-500 transition " >
           {state === "login" ? "Login" : "Sign up"}
         </button>
 
         <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-400 text-sm mt-3 mb-11 cursor-pointer" >
           {state === "login" ? "Don't have an account?" : "Already have an account?"}
-          <span className="text-indigo-400 hover:underline ml-1">click here</span>
+          <span className="text-green-400 hover:underline ml-1">click here</span>
         </p>
       </form>
     </div>
